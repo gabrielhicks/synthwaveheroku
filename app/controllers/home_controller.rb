75fe0a798993acf344485ad24c26f375dd47ca02
gabrielhicks <gabrielhicks@mail.com>
2020-10-26 11:32:12 -0400

    added redirect

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+    def index
+        redirect_to "https://gabrielhicks.github.io/synthwave-frontend/"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :songs
   resources :users
   resources :comments
+  root to: 'home#index'
 end
